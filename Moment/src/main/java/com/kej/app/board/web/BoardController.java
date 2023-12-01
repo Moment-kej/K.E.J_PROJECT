1c60447f90cdb41479273028150615f359997631
user <user@DESKTOP-8IL4IHI>
2023-12-01 16:34:56 +0900

    게시판 등록 페이지 css 수정완료

--- Moment/src/main/java/com/kej/app/board/web/BoardController.java
+++ Moment/src/main/java/com/kej/app/board/web/BoardController.java
@@ -2,6 +2,7 @@ package com.kej.app.board.web;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kej.app.board.service.BoardService;
