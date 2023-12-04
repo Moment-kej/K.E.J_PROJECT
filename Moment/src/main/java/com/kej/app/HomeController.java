cf5f698e9a9ba1c4bfb37ff823e71ae717916082
JjungEeunAae <wjddmsdo77@naver.com>
2023-12-05 00:43:26 +0900

    메인 --> 네이버뉴스 삽입중

--- Moment/src/main/java/com/kej/app/HomeController.java
+++ Moment/src/main/java/com/kej/app/HomeController.java
@@ -7,10 +7,15 @@ import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import com.kej.app.common.service.CommonService;
 
@@ -20,25 +25,48 @@ import com.kej.app.common.service.CommonService;
 @Controller
 public class HomeController {
 	@Autowired CommonService service;
-	
-//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-//		logger.info("Welcome home! The client locale is {}.", locale);
+//	private static String CLIENT_ID = "uzFgU5Xq_6esU6400qi2";
+//	private static String CLIENT_SECRET = "2zYFlRC1sO";
+//	
+////	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//	
+//	/**
+//	 * Simply selects the home view to render by returning its name.
+//	 */
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+////		logger.info("Welcome home! The client locale is {}.", locale);
+////		
+////		Date date = new Date();
+////		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+////		
+////		String formattedDate = dateFormat.format(date);
+////		
+////		model.addAttribute("serverTime", formattedDate );
+//		model.addAttribute("code", service.getCodes("CO","EM"));
 //		
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//		
-//		String formattedDate = dateFormat.format(date);
-//		
-//		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("code", service.getCodes("CO","EM"));
-		
-		return "comm/main";
-	}
+//		// 네이버 뉴스 API 호출
+//        String apiUrl = "https://openapi.naver.com/v1/search/news.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=date";
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        // 헤더 설정
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("X-Naver-Client-Id", CLIENT_ID);
+//        headers.set("X-Naver-Client-Secret", CLIENT_SECRET);
+//        HttpEntity<String> entity = new HttpEntity<>(headers);
+//
+//        // API 호출 및 결과 처리
+//        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+//        String responseBody = response.getBody();
+//
+//        // 결과를 모델에 추가
+//        model.addAttribute("newsApiResponse", responseBody);
+//        
+//        System.out.println(responseBody);
+//        
+//        //<c:out value="${newsApiResponse}" escapeXml="false" />
+//        // 뉴스를 표시할 JSP 페이지의 이름을 반환
+//		return "comm/main";
+//	}
 	
 }
--- Moment/src/main/java/com/kej/app/common/service/impl/CommonServiceImpl.java
+++ Moment/src/main/java/com/kej/app/common/service/impl/CommonServiceImpl.java
@@ -5,17 +5,27 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.kej.app.board.service.vo.Criteria;
 import com.kej.app.common.mapper.CommonMapper;
 import com.kej.app.common.service.CommonService;
 import com.kej.app.common.service.vo.CodeVO;
+import com.kej.app.common.service.vo.NeverNews;
+import com.kej.app.common.service.vo.NewsVO;
 
 @Service
 public class CommonServiceImpl implements CommonService{
-	@Autowired
-	CommonMapper commonMapper;
-
+	@Autowired CommonMapper commonMapper;
+	private static String CLIENT_ID = "uzFgU5Xq_6esU6400qi2";
+	private static String CLIENT_SECRET = "2zYFlRC1sO";
+	private static int DISPLAY_COUNT = 10;	// 한 페이지에 보여줄 뉴스 개수
+    
 	// 여러 그룹 코드들의 상세코드 조회
 	@Override
 	public Map<String, List<CodeVO>> getCodes(String... commList) {
@@ -24,9 +34,42 @@ public class CommonServiceImpl implements CommonService{
 			map.put(coCd, commonMapper.selectCode(coCd));
 		};
 		return map;
+	}
+
+	@Override
+	public List<NewsVO> news(int page, String query) {
+		int start = (page - 1) * DISPLAY_COUNT + 1;
+		
+		// 네이버 뉴스 API 호출
+        String apiUrl = "https://openapi.naver.com/v1/search/news.json?query="
+		        		+ query
+		        		+ "&display="
+        				+ DISPLAY_COUNT
+        				+ "&start="
+        				+ start
+        				+ "&sort=date";
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", CLIENT_ID);
+        headers.set("X-Naver-Client-Secret", CLIENT_SECRET);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // API 호출 및 결과 처리
+        ResponseEntity<NeverNews> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, NeverNews.class);
+        
+        List<NewsVO> list = response.getBody().getItems();
+
+		return list;
+	}
+
+	@Override
+	public int getTotalPages(String query) {
+		int totalNewsCount = 100;
+		return totalNewsCount;
 	};
-	
-	
+
 	
 	
 	
