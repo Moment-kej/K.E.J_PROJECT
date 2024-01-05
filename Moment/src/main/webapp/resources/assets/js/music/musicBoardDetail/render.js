cc68b0a752111cac1f0cee72a65ab747f553ac10
mjin0302 <alswls7887@gmail.com>
2024-01-06 02:16:55 +0900

    댓글 수정 value값 확인하기

--- Moment/src/main/webapp/resources/assets/js/music/musicBoardDetail/render.js
+++ Moment/src/main/webapp/resources/assets/js/music/musicBoardDetail/render.js
@@ -4,8 +4,8 @@ export const createReplyBox = (data) => {
    const comment_area_box = document.getElementById("comment_area_box");
 
    data.map((item) => {
-      console.log(item);
       let writeDt = formatTime_hhmmss(item.writeDt)
+      
       const listElement = createAndAppendElement(comment_area_box, "li", { class: "commentItem pt-1" });
       const commentArea = createAndAppendElement(listElement, "div", { class: "comment_area" });
       const firstContainer = createAndAppendElement(commentArea, "div", { class: "d-flex justify-content-between align-items-center" });
@@ -16,9 +16,9 @@ export const createReplyBox = (data) => {
       const replyInfoArea = createAndAppendElement(replyInfoBox, "div", { id: "replyInfoArea" });
       createAndAppendElement(replyInfoArea, "span", { class: "replyInfoID" }, item.id);
       createAndAppendElement(replyInfoArea, "span", { class : "replyInfoDate" }, writeDt)
-      const btnContainer = createAndAppendElement(firstContainer, "div", { class: "commentBtnBox" });
+      const btnContainer = createAndAppendElement(firstContainer, "div", { class: "commentBtnBox", "data-value": item.replyNo });
       createAndAppendElement(btnContainer, "button", { class: "modifyBtnBox" }, '<i class="fa-solid fa-pen-to-square commentModifyBtn"></i>');
-      createAndAppendElement(btnContainer, "button", { class: "deleteBtnBox", "data-value": item.replyNo }, '<i class="fa-solid fa-trash commentDelBtn"></i>');
+      createAndAppendElement(btnContainer, "button", { class: "deleteBtnBox" }, '<i class="fa-solid fa-trash commentDelBtn"></i>');
       const commentContent = createAndAppendElement(commentArea, "div", { class: "commentContent" });
       createAndAppendElement(commentContent, "span", {}, item.content)
       createAndAppendElement(commentArea, "div", { class: "commentWirterBtnBox" }, '<button type="button" class="commentWriteBtn">답글작성</button>');
@@ -28,7 +28,7 @@ export const createReplyBox = (data) => {
       const commentWriter = createAndAppendElement(commentWriteBox, "div", { class: "commentWriter" });
       const commentInbox = createAndAppendElement(commentWriter, "div", { class: "comment_inbox" });
       createAndAppendElement(commentInbox, "span", { class: "comment_inbox_name" }, item.id);
-      createAndAppendElement(commentInbox, "textarea", { placeholder: "댓글을 남겨보세요 (여기는 버튼 누르면 나타나는 거임)" });
+      createAndAppendElement(commentInbox, "textarea", { placeholder: "댓글을 남겨보세요." });
       const commentAttach = createAndAppendElement(commentWriter, "div", { class: "comment_attach d-flex justify-content-between align-items-center" });
       const commentBoxWriteCount = createAndAppendElement(commentAttach, "div", { class: "comment_box_write_count" });
       createAndAppendElement(commentBoxWriteCount, "strong", { class: "fontSizeSmall comment_box_count_num" }, "0");
